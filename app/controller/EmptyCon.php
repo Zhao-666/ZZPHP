a67a41d05562c2adebedb96a25c07b7b1eeed698
Zhao-666 <2268699370@qq.com>
2018-02-26 15:22:47 +0800

    将默认控制器文件夹ctrl改为controller

--- app/controller/EmptyCon.php
+++ app/controller/EmptyCon.php
@@ -16,8 +16,8 @@ class EmptyCon
         p("找不到控制器");
     }
 
-//    public function _empty($method)
-//    {
-//        p('当前操作名：' . $method);
-//    }
+    public function _empty($method)
+    {
+        p('当前操作名：' . $method);
+    }
 }
